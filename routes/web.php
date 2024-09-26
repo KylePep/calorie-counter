4a10f02c067c4f58e97f65606f6d75e0ea13767a
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-09-26 02:52:01 -0600

    Created FoodController, imported FoodController into web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FoodController;
 use App\Http\Controllers\FoodDataController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
