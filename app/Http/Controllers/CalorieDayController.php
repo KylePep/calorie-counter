6d1147e7fa0413638655c9d3983771225682b603
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-09-30 14:36:51 -0600

    update calorieDay

--- app/Http/Controllers/CalorieDayController.php
+++ app/Http/Controllers/CalorieDayController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\CalorieDay;
 use App\Http\Requests\StoreCalorieDayRequest;
 use App\Http\Requests\UpdateCalorieDayRequest;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 
 class CalorieDayController extends Controller
 {
@@ -27,7 +31,7 @@ class CalorieDayController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCalorieDayRequest $request)
+    public function store(Request $request)
     {
         //
     }
@@ -53,7 +57,21 @@ class CalorieDayController extends Controller
      */
     public function update(UpdateCalorieDayRequest $request, CalorieDay $calorieDay)
     {
-        //
+
+        $validated = $request->validate([
+            'goal' => ['integer'],
+            'count' => ['integer'],
+            'food_items'=> ['array'],
+        ]);
+
+        $calorieDay->goal = $validated['goal'] ?? $calorieDay->goal;
+        $calorieDay->count += $validated['count'] ?? 0;
+
+        $existingFoodItems = json_decode($calorieDay->food_items,true) ?? [];
+        $calorieDay->food_items = json_encode(array_merge($existingFoodItems, $validated['food_items']));
+
+        return $calorieDay;
+
     }
 
     /**
