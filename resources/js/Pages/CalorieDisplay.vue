b4541ef182b94e38a3cbdfb248c2ba939ba249f4
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-10-15 16:55:33 -0600

    Created CalorieDisplay component, implemented on dashboard

---
+++ resources/js/Pages/CalorieDisplay.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from "vue";
+
+
+const props = defineProps(['calorieGoal', 'calorieCount'])
+
+const cellCount = ref(props.calorieGoal / 100)
+const calorieCountRows = ref(Math.ceil(props.calorieGoal / 1000))
+
+</script>
+
+<template>
+
+  <section class=" z-10 bg-white p-1 text-center border-4 rounded-lg border-black/25"
+    :class="calorieCount < calorieGoal ? 'sticky top-16' : ''">
+
+    <div v-for="row in calorieCountRows" :key="row" class="  grid grid-rows-1 grid-flow-col gap-1 mb-1">
+      <div v-for="index in 10" :key="index"
+        :class="[((row - 1) * 10 + index) * 100 <= calorieCount && index + ((row - 1) * 10) <= cellCount ? 'bg-green-500 text-transparent' : 'bg-gray-300 text-black/50', index + ((row - 1) * 10) <= cellCount ? '' : 'bg-black/25 text-transparent']"
+        class="min-h-10 text-sm flex flex-col justify-center duration-300">
+        100
+      </div>
+    </div>
+  </section>
+
+  <section v-if="calorieCount > calorieGoal"
+    class="sticky top-16 z-20 grid grid-rows-2 grid-flow-col gap-1 bg-white p-1 text-center border-4 rounded-lg border-black/25">
+    <div v-for="index in 40" :key="index"
+      :class="[index * 50 + calorieGoal <= calorieCount ? 'bg-yellow-500' : 'bg-gray-300']"
+      class="min-h-10 duration-300">
+    </div>
+  </section>
+
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -10,6 +10,7 @@ import Pop from "@/utils/Pop.js";
 import { Head, } from '@inertiajs/vue3';
 import axios from "axios";
 import { computed, ref } from "vue";
+import CalorieDisplay from './CalorieDisplay.vue'
 
 const props = defineProps(['account', 'calorieDay', 'with_fdcId', 'without_fdcId'])
 
@@ -22,8 +23,6 @@ const goal = computed(() => calorieDay.value?.goal ?? 2000);
 const calorieCount = computed(() => calorieDay.value?.count ?? 0);
 
 const calorieGoal = ref(goal)
-const cellCount = ref(calorieGoal.value / 100)
-const calorieCountRows = ref(Math.ceil(calorieGoal.value / 1000))
 
 const showEditForm = ref(false);
 const showUsdaForm = ref(false);
@@ -111,7 +110,7 @@ function handleExtraButton(item, action, type) {
                     <li>Edit Usda form, loading screen</li>
                     <li class="line-through text-gray-400">Scrolling text needs adjustment on consumed list</li>
                     <li class="line-through text-gray-400">Consumed List cursor-pointer</li>
-                    <li>Profile should have recorded account details editable</li>
+                    <li class="line-through text-gray-400">Profile should have recorded account details editable</li>
                     <li class="line-through text-gray-400">FoodList no search, remove hover</li>
                 </ul>
                 <ul class="list-disc">
@@ -137,26 +136,7 @@ function handleExtraButton(item, action, type) {
                 </p>
             </section>
 
-
-            <section class=" z-10 bg-white p-1 text-center border-4 rounded-lg border-black/25"
-                :class="calorieCount < calorieGoal ? 'sticky top-16' : ''">
-
-                <div v-for="row in calorieCountRows" :key="row" class="  grid grid-rows-1 grid-flow-col gap-1 mb-1">
-                    <div v-for="index in 10" :key="index"
-                        :class="[((row - 1) * 10 + index) * 100 <= calorieCount && index + ((row - 1) * 10) <= cellCount ? 'bg-green-500 text-transparent' : 'bg-gray-300 text-black/50', index + ((row - 1) * 10) <= cellCount ? '' : 'bg-black/25 text-transparent']"
-                        class="min-h-10 text-sm flex flex-col justify-center duration-300">
-                        100
-                    </div>
-                </div>
-            </section>
-
-            <section v-if="calorieCount > calorieGoal"
-                class="sticky top-16 z-20 grid grid-rows-2 grid-flow-col gap-1 bg-white p-1 text-center border-4 rounded-lg border-black/25">
-                <div v-for="index in 40" :key="index"
-                    :class="[index * 50 + calorieGoal <= calorieCount ? 'bg-yellow-500' : 'bg-gray-300']"
-                    class="min-h-10 duration-300">
-                </div>
-            </section>
+            <CalorieDisplay :calorieGoal="calorieGoal" :calorieCount="calorieCount" />
 
             <section v-if="props.account && calorieDay.food_items.length">
                 <ConsumedList :dayItems="calorieDay.food_items" @remove-food-item="removeAndSubtractFoodItem" />
