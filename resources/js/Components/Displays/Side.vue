f877aca6c398ea546eff835f4212bb4d459c11f4
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-11-01 16:41:38 -0600

    fixed padding on side component

--- resources/js/Components/Displays/Side.vue
+++ resources/js/Components/Displays/Side.vue
@@ -16,7 +16,7 @@ const sideClasses = computed(() => {
 <template>
 
   <section class="border-light lg:pt-0 text-sm px-6 pt-4 sm:pt-0">
-    <div :class="sideClasses" class="flex flex-col space-y-4 px-0 sm:px-6 lg:px-8 py-0 sm:py-6 ">
+    <div :class="sideClasses" class="flex flex-col space-y-4 px-0  py-0 sm:py-6 ">
       <slot />
     </div>
   </section>
