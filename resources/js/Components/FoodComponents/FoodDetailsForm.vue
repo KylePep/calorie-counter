cbcfb00992d9c31c2d043de1182b72725afe9aa6
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-11-04 16:01:41 -0700

    resized text on detailForms, changed columns and spacing of absolute nameTag

--- resources/js/Components/FoodComponents/FoodDetailsForm.vue
+++ resources/js/Components/FoodComponents/FoodDetailsForm.vue
@@ -35,12 +35,13 @@ const unitName = computed(() => {
     <div class="flex">
       <div class="basis-3/5 me-3">
         <InputLabel for="description" value="Name or description"></InputLabel>
-        <TextInput id="description" v-model="form.description" class="w-full" ref="nameInput" required></TextInput>
+        <TextInput id="description" v-model="form.description" class="w-full text-sm" ref="nameInput" required>
+        </TextInput>
         <InputError :message="form.errors.description"></InputError>
       </div>
       <div class="basis-2/5">
         <InputLabel for="category" value="Category"></InputLabel>
-        <TextInput id="category" v-model="form.foodCategory" class="w-full" required></TextInput>
+        <TextInput id="category" v-model="form.foodCategory" class="w-full text-sm" required></TextInput>
         <InputError :message="form.errors.foodCategory"></InputError>
 
       </div>
@@ -50,7 +51,7 @@ const unitName = computed(() => {
 
       <div class="sm:basis-3/5 me-3">
         <InputLabel for="calories" value="Amount of calories"></InputLabel>
-        <NumberInput id="calories" v-model="form.calories" class="w-full" required></NumberInput>
+        <NumberInput id="calories" v-model="form.calories" class="w-full text-sm" required></NumberInput>
         <InputError :message="form.errors.calories"></InputError>
       </div>
 
@@ -58,7 +59,7 @@ const unitName = computed(() => {
 
         <div class="w-full relative">
           <InputLabel for="servingSize" value="Serving Size"></InputLabel>
-          <NumberInput id="servingSize" v-model="form.servingSize" class="w-full" required></NumberInput>
+          <NumberInput id="servingSize" v-model="form.servingSize" class="w-full text-sm" required></NumberInput>
           <span class="absolute right-0 bottom-0 pb-3 pe-3 font-bold text-black/50 text-xs">{{ unitName
             }}</span>
           <InputError :message="form.errors.servingSize"></InputError>
@@ -86,14 +87,14 @@ const unitName = computed(() => {
       <div class="flex ">
         <div class="basis-1/2 me-3">
           <InputLabel for="brandName" value="Brand Name"></InputLabel>
-          <TextInput id="brandName" v-model="form.brandName" class="w-full"></TextInput>
+          <TextInput id="brandName" v-model="form.brandName" class="w-full text-sm"></TextInput>
           <InputError :message="form.errors.description"></InputError>
 
         </div>
         <div class="basis-1/2">
 
           <InputLabel for="brandOwner" value="Brand Owner"></InputLabel>
-          <TextInput id="brandOwner" v-model="form.brandOwner" class="w-full"></TextInput>
+          <TextInput id="brandOwner" v-model="form.brandOwner" class="w-full text-sm"></TextInput>
           <InputError :message="form.errors.brandOwner"></InputError>
 
         </div>
@@ -107,7 +108,7 @@ const unitName = computed(() => {
       <InputLabel for="ingredients" value="Ingredients">
       </InputLabel>
       <textarea id="ingredients" v-model="form.ingredients" name="ingredients"
-        class="w-full border-gray-300 focus:border-accent focus:ring-accent rounded-md shadow-sm"
+        class="w-full text-sm border-gray-300 focus:border-accent focus:ring-accent rounded-md shadow-sm"
         style="resize: none;"></textarea>
       <InputError :message="form.errors.ingredients"></InputError>
     </div>
@@ -115,12 +116,12 @@ const unitName = computed(() => {
     <div>
       <button type="button" @click="showNutrients = !showNutrients" class="rounded-lg bg-gray-300 px-3 py-2">Nutrients
         <span :class="[!showNutrients ? 'mdi mdi-menu-down' : 'mdi mdi-menu-up']"></span> </button>
-      <div v-if="showNutrients" class="grid grid-cols-3 text-center gap-2 mt-3">
+      <div v-if="showNutrients" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 text-center gap-2 mt-3">
         <div v-for="(nutrient, index) in form.foodNutrients" :key="index">
           <InputLabel :for="nutrient.nutrientName" :value="nutrient.nutrientName"></InputLabel>
           <div class="relative">
             <NumberInput :id="nutrient.nutrientName" v-model="form.foodNutrients[index].value"
-              class="w-full text-center">
+              class="w-full text-sm text-center">
             </NumberInput>
             <div
               class="absolute flex items-center justify-center pe-1 sm:pe-3 h-full right-0 top-0 text-black/50 text-xs sm:text-base font-bold">
--- resources/js/Components/FoodComponents/UsdaFoodDetailsForm.vue
+++ resources/js/Components/FoodComponents/UsdaFoodDetailsForm.vue
@@ -46,14 +46,14 @@ const modifier = computed(() => {
     <div class="flex  ">
       <div class="basis-3/5 me-3">
         <InputLabel for="description" value="Name or description"></InputLabel>
-        <TextInput id="description" v-model="form.description" :class="loadingClasses" class="w-full" ref="nameInput"
-          required>
+        <TextInput id="description" v-model="form.description" :class="loadingClasses" class="w-full  text-sm"
+          ref="nameInput" required>
         </TextInput>
         <InputError :message="form.errors.description"></InputError>
       </div>
       <div class="basis-2/5">
         <InputLabel for="category" value="Category"></InputLabel>
-        <TextInput id="category" v-model="form.foodCategory" :class="loadingClasses" class="w-full" required>
+        <TextInput id="category" v-model="form.foodCategory" :class="loadingClasses" class="w-full  text-sm" required>
         </TextInput>
         <InputError :message="form.errors.foodCategory"></InputError>
 
@@ -64,7 +64,7 @@ const modifier = computed(() => {
 
       <div class="basis-3/5 me-3">
         <InputLabel for="calories" value="Amount of calories"></InputLabel>
-        <div id="calories" :class="loadingClasses" class="rounded px-2 py-1 mt-2">
+        <div id="calories" :class="loadingClasses" class="rounded px-2 py-1 mt-2 text-sm">
           {{ Math.round(form.realCalories * modifier) }}
         </div>
       </div>
@@ -75,7 +75,7 @@ const modifier = computed(() => {
             <InputLabel for="portion" value="Portion"></InputLabel>
             <div class="mt-2">
               <select v-model="form.portionModifier" id="portion" name="portion" :class="loadingClasses"
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                class="block w-full rounded-md border-0 py-1.5  text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 <option value="100">100g</option>
                 <option v-for="portion in form.foodPortions" :value=portion.gramWeight> {{ portion.amount }} {{
                   portion.modifier }} ({{ portion.gramWeight }}g)</option>
@@ -86,10 +86,14 @@ const modifier = computed(() => {
 
           <div v-else>
             <InputLabel for="servingSize" value="Serving Size"></InputLabel>
-            <NumberInput id="servingSize" v-model="form.portionModifier" :class="loadingClasses" class="w-full"
+            <NumberInput id="servingSize" v-model="form.portionModifier" :class="loadingClasses" class="w-full  text-sm"
               required></NumberInput>
-            <span class="absolute right-0 bottom-0 pb-3 pe-3 font-bold text-black/50 text-xs">{{ unitName
-              }}</span>
+            <span class="absolute hidden sm:block right-0 bottom-0 pb-3 pe-3 font-bold text-black/50 text-sm">{{
+              unitName
+            }}</span>
+            <span class="absolute block sm:hidden right-0 bottom-0 pb-3 pe-3 font-bold text-black/50 text-sm">{{
+              formData.servingSizeUnit
+            }}</span>
             <InputError :message="form.errors.servingSize"></InputError>
           </div>
         </div>
@@ -103,14 +107,15 @@ const modifier = computed(() => {
       <div class="flex ">
         <div class="basis-1/2 me-3">
           <InputLabel for="brandName" value="Brand Name"></InputLabel>
-          <TextInput id="brandName" v-model="form.brandName" :class="loadingClasses" class="w-full"></TextInput>
+          <TextInput id="brandName" v-model="form.brandName" :class="loadingClasses" class="w-full text-sm"></TextInput>
           <InputError :message="form.errors.brandName"></InputError>
 
         </div>
         <div class="basis-1/2">
 
           <InputLabel for="brandOwner" value="Brand Owner"></InputLabel>
-          <TextInput id="brandOwner" v-model="form.brandOwner" :class="loadingClasses" class="w-full"></TextInput>
+          <TextInput id="brandOwner" v-model="form.brandOwner" :class="loadingClasses" class="w-full text-sm">
+          </TextInput>
           <InputError :message="form.errors.brandOwner"></InputError>
 
         </div>
@@ -124,7 +129,7 @@ const modifier = computed(() => {
       <InputLabel for="ingredients" value="Ingredients">
       </InputLabel>
       <textarea id="ingredients" v-model="form.ingredients" name="ingredients" :class="loadingClasses"
-        class="w-full border-gray-300 focus:border-accent focus:ring-accent rounded-md shadow-sm"
+        class="w-full text-sm border-gray-300 focus:border-accent focus:ring-accent rounded-md shadow-sm"
         style="resize: none;"></textarea>
       <InputError :message="form.errors.ingredients"></InputError>
     </div>
@@ -133,13 +138,19 @@ const modifier = computed(() => {
       <button type="button" @click="showNutrients = !showNutrients" :class="loadingClasses"
         class="rounded-lg bg-gray-300 px-3 py-2">Nutrients
         <span :class="[!showNutrients ? 'mdi mdi-menu-down' : 'mdi mdi-menu-up']"></span> </button>
-      <div v-if="showNutrients" class="grid grid-cols-3 text-center gap-2 mt-3">
+      <div v-if="showNutrients" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 text-center gap-2 mt-3">
         <div v-for="(nutrient, index) in form.foodNutrients" :key="index" class="relative">
           <InputLabel :for="nutrient.nutrientName" :value="nutrient.nutrientName"></InputLabel>
-          <NumberInput :id="nutrient.nutrientName" v-model="form.foodNutrients[index].value" :class="loadingClasses"
-            class="w-full text-center">
-          </NumberInput>
-          <div class="absolute right-0 top-1/2 pe-3 text-black/50 font-bold">{{ nutrient.unitName }}</div>
+          <div class="relative">
+            <NumberInput :id="nutrient.nutrientName" v-model="form.foodNutrients[index].value" :class="loadingClasses"
+              class="w-full text-sm text-center">
+            </NumberInput>
+            <div
+              class="absolute flex items-center justify-center pe-1 sm:pe-3 h-full right-0 top-0 text-black/50 text-sm sm:text-base font-bold">
+              {{
+                nutrient.unitName }}
+            </div>
+          </div>
         </div>
       </div>
 
