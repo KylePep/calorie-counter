740a56cca2d8c1de5e694c60ab88462c0dcee8f4
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-11-04 14:16:32 -0700

    renamed SearchMode->DisplayMode to better represent function

--- resources/js/Pages/History/Show.vue
+++ resources/js/Pages/History/Show.vue
@@ -6,7 +6,7 @@ import NewDay from "./Partials/NewDay.vue";
 import Side from "@/Components/Displays/Side.vue";
 import SearchBar from "./Partials/SearchBar.vue";
 import { onMounted, ref, watch } from "vue";
-import SearchMode from './Partials/SearchMode.vue'
+import SearchMode from './Partials/DisplayMode.vue'
 
 const props = defineProps(['account', 'calorieDays', 'results',]);
 
