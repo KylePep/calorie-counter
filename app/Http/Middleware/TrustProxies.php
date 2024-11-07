40a4ff7d642301be564f7760766c80620fd66733
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-11-07 16:15:34 -0700

    Inertia middleware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,7 +1,10 @@
 <?php
+ 
 namespace App\Http\Middleware;
+ 
 use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
+use Inertia\Middleware;
+ 
 class TrustProxies extends Middleware
 {
     protected $proxies = '*';
