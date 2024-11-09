37cef323e0ce7c4638ec5037cd71b639a2caecdd
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-11-08 22:52:16 -0700

    Friend's food search mockup

--- resources/js/Pages/Food.vue
+++ resources/js/Pages/Food.vue
@@ -9,6 +9,7 @@ import FoodEditModal from "@/Components/FoodComponents/FoodEditModal.vue";
 import Pop from "@/utils/Pop.js";
 import UsdaFoodEditModal from "@/Components/FoodComponents/UsdaFoodEditModal.vue";
 import Side from "@/Components/Displays/Side.vue";
+import TextInput from "@/Components/Form/TextInput.vue";
 
 const props = defineProps(['account', 'with_fdcId', 'without_fdcId']);
 
@@ -84,6 +85,44 @@ function handleExtraButton(item, action, type) {
       </div>
     </section>
 
+    <section>
+      <div class=" bg-main border border-light rounded">
+        <div class="grid grid-cols-4">
+          <div class="col-span-4 text-dark-text font-bold text-lg px-2 pt-2">Friend's food</div>
+          <TextInput class="col-span-3" />
+          <p class="flex items-center justify-center bg-accent text-dark-text font-bold">Find a Friend</p>
+        </div>
+
+        <select id="activity" name="activity"
+          class=" w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6">
+          <option value=1>Select a friend</option>
+          <option value=1.2>Moose</option>
+          <option value=1.375>Satchel</option>
+          <option value=1.465>Oskar</option>
+        </select>
+
+        <div class="bg-light grid grid-cols-4 gap-4 py-2 px-1 text-center font-bold">
+          <button class="bg-main px-2 py-1 rounded">breakfast</button>
+          <button class="bg-main px-2 py-1 rounded">lunch</button>
+          <button class="bg-main px-2 py-1 rounded">dinner</button>
+          <button class="bg-main px-2 py-1 rounded">snack</button>
+        </div>
+        <div class="grid grid-cols-6 gap-2">
+
+          <button class="bg-main border border-light mdi mdi-menu-left text-7xl"></button>
+
+          <div v-for="index in 4"
+            class="h-20 flex flex-col items-center justify-center text-center bg-main border border-light text-xs">
+            <p> {{ index }}</p>
+            <p>Food Item</p>
+
+          </div>
+          <button class="bg-main border border-light mdi mdi-menu-right text-7xl"></button>
+
+        </div>
+      </div>
+    </section>
+
     <section v-if="props.account" class="block 2xl:hidden">
       <CreateFood />
     </section>
@@ -105,7 +144,7 @@ function handleExtraButton(item, action, type) {
     </section>
 
     <template #rightSide>
-      <Side side="right" class="hidden lg:block" v-if="props.account">
+      <Side side="right" class="hidden 2xl:block" v-if="props.account">
         <CreateFood />
       </Side>
     </template>
