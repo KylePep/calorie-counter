732c07075b98f0cb5f2e8b6a3e58db90a4916041
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-11-20 16:05:09 -0700

    Search bar on FoodMenu component

--- resources/js/Components/Menu/FoodMenu.vue
+++ resources/js/Components/Menu/FoodMenu.vue
@@ -4,6 +4,7 @@ import Dropdown from "../Form/Dropdown.vue";
 import FoodCardButton from "../FoodComponents/FoodCardButton.vue";
 import { useForm, usePage } from "@inertiajs/vue3";
 import Pop from "@/utils/Pop.js";
+import TextInput from "../Form/TextInput.vue";
 
 const props = defineProps(['list', 'calorieDay']);
 
@@ -12,10 +13,18 @@ const isDashboard = page.url.includes('dashboard');
 const isCalorieDay = page.url.includes('calorie-day');
 const filter = ref('All');
 
+const form = useForm({
+  search: ''
+})
+
 const filteredList = computed(() => {
-  if (filter.value != 'All') {
-    return props.list.filter((li) => li.foodCategory == filter.value.toLocaleLowerCase());
-  } else return props.list;
+  return props.list
+    .filter((li) => {
+      return filter.value === 'All' || li.foodCategory === filter.value.toLowerCase();
+    })
+    .filter((li) => {
+      return !form.search || li.description.toLowerCase().includes(form.search.toLowerCase());
+    })
 });
 
 function useItem(foodItem) {
@@ -79,39 +88,41 @@ async function deleteItem(foodItem) {
       Filter and search your foods.
     </h2>
 
-    <section>
-      <div class="col-span-3 sm:col-span-2 flex items-center bg-light rounded">
-        <Dropdown align="left" width="100" class="w-full">
+    <section class="grid lg:grid-cols-2 lg:gap-x-2 bg-light rounded p-1">
 
-          <template #trigger>
-            <button type="button"
-              class="w-full h-8 flex justify-between items-center px-2 py-2  rounded text-xs font-bold text-dark-text hover:text-accent uppercase hover:bg-dark transition ease-in-out duration-150">
-              <p class="flex-1 flex items-center text-center">
-                {{ filter }}
-              </p>
-              <i class="mdi mdi-menu-down text-lg"></i>
-            </button>
-          </template>
+      <TextInput v-model="form.search" placeholder="Search by description" class="h-8 w-full" />
 
-          <template #content>
-            <div class="flex flex-col p-2 bg-light rounded text-dark-text text-xs">
-              <button class="text-start p-1" :class="[filter == 'All' ? 'border border-black/25 rounded' : '']"
-                type="button" @click="filter = 'All'">All</button>
-              <button class="text-start p-1" :class="[filter == 'Breakfast' ? 'border border-black/25 rounded' : '']"
-                type="button" @click="filter = 'Breakfast'">Breakfast</button>
-              <button class="text-start p-1" :class="[filter == 'Lunch' ? 'border border-black/25 rounded' : '']"
-                type="button" @click="filter = 'Lunch'">Lunch</button>
-              <button class="text-start p-1" :class="[filter == 'Dinner' ? 'border border-black/25 rounded' : '']"
-                type="button" @click="filter = 'Dinner'">Dinner</button>
-              <button class="text-start p-1" :class="[filter == 'Beverage' ? 'border border-black/25 rounded' : '']"
-                type="button" @click="filter = 'Beverage'">Beverage</button>
-              <button class="text-start p-1" :class="[filter == 'Snack' ? 'border border-black/25 rounded' : '']"
-                type="button" @click="filter = 'Snack'">Snack</button>
-            </div>
-          </template>
+      <Dropdown align="left" width="100" class="w-full">
 
-        </Dropdown>
-      </div>
+        <template #trigger>
+          <button type="button"
+            class="w-full h-8 flex justify-between items-center px-2 py-2  rounded text-xs font-bold text-dark-text hover:text-accent uppercase hover:bg-dark transition ease-in-out duration-150">
+            <p class="flex-1 flex items-center text-center">
+              {{ filter }}
+            </p>
+            <i class="mdi mdi-menu-down text-lg"></i>
+          </button>
+        </template>
+
+        <template #content>
+          <div class="flex flex-col p-2 bg-light rounded text-dark-text text-xs">
+            <button class="text-start p-1" :class="[filter == 'All' ? 'border border-black/25 rounded' : '']"
+              type="button" @click="filter = 'All'">All</button>
+            <button class="text-start p-1" :class="[filter == 'Breakfast' ? 'border border-black/25 rounded' : '']"
+              type="button" @click="filter = 'Breakfast'">Breakfast</button>
+            <button class="text-start p-1" :class="[filter == 'Lunch' ? 'border border-black/25 rounded' : '']"
+              type="button" @click="filter = 'Lunch'">Lunch</button>
+            <button class="text-start p-1" :class="[filter == 'Dinner' ? 'border border-black/25 rounded' : '']"
+              type="button" @click="filter = 'Dinner'">Dinner</button>
+            <button class="text-start p-1" :class="[filter == 'Beverage' ? 'border border-black/25 rounded' : '']"
+              type="button" @click="filter = 'Beverage'">Beverage</button>
+            <button class="text-start p-1" :class="[filter == 'Snack' ? 'border border-black/25 rounded' : '']"
+              type="button" @click="filter = 'Snack'">Snack</button>
+          </div>
+        </template>
+
+      </Dropdown>
+
     </section>
 
     <section v-if="filteredList.length == 0"
