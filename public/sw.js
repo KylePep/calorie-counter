5fd49310e488b50ef41889cc722d78fa0423ac70
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-11-21 17:42:43 -0700

    Testing scan feature

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,10 @@
+const filesToCache = [
+    '/',
+    '/offline.html'
+];
+
 const preLoad = function () {
     return caches.open("offline").then(function (cache) {
-        // caching index and important routes
         return cache.addAll(filesToCache);
     });
 };
@@ -9,27 +13,22 @@ self.addEventListener("install", function (event) {
     event.waitUntil(preLoad());
 });
 
-const filesToCache = [
-    '/',
-    '/offline.html'
-];
-
 const checkResponse = function (request) {
-    return new Promise(function (fulfill, reject) {
-        fetch(request).then(function (response) {
-            if (response.status !== 404) {
-                fulfill(response);
-            } else {
-                reject();
-            }
-        }, reject);
+    return fetch(request).then(function (response) {
+        if (response.status !== 404) {
+            return response;
+        } else {
+            throw new Error("Not Found");
+        }
     });
 };
 
 const addToCache = function (request) {
     return caches.open("offline").then(function (cache) {
         return fetch(request).then(function (response) {
-            return cache.put(request, response);
+            if (response.ok) {
+                return cache.put(request, response.clone());
+            }
         });
     });
 };
@@ -37,20 +36,21 @@ const addToCache = function (request) {
 const returnFromCache = function (request) {
     return caches.open("offline").then(function (cache) {
         return cache.match(request).then(function (matching) {
-            if (!matching || matching.status === 404) {
-                return cache.match("offline.html");
-            } else {
-                return matching;
-            }
+            return matching || cache.match("/offline.html");
         });
     });
 };
 
 self.addEventListener("fetch", function (event) {
-    event.respondWith(checkResponse(event.request).catch(function () {
-        return returnFromCache(event.request);
-    }));
-    if(!event.request.url.startsWith('http')){
+    if (event.request.url.startsWith("http")) {
+        event.respondWith(
+            checkResponse(event.request).catch(function () {
+                return returnFromCache(event.request);
+            })
+        );
+
         event.waitUntil(addToCache(event.request));
+    } else {
+        console.warn("Unsupported request scheme:", event.request.url);
     }
 });
