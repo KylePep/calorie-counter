bf20cc68a226cd5e02d3bccaaeead01f579047f4
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-11-27 12:36:15 -0700

    env as path

--- config/filesystems.php
+++ config/filesystems.php
@@ -61,7 +61,7 @@ return [
             'driver' => 'gcs',
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'), 
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET'), 
-            'key_file' => storage_path('GOOGLE_CLOUD_KEY_FILE'),
+            'key_file' => storage_path(env('GOOGLE_CLOUD_KEY_FILE')),
             'prefix' => env('GOOGLE_CLOUD_STORAGE_PREFIX', null),
             'visibility' => null, // Disable ACLs
         ],
