56543f84c1ac7a5ac0579659479bd2aac99e8f58
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-11-27 14:36:00 -0700

    startScanner over restart

--- resources/js/Components/BarcodeScanner.vue
+++ resources/js/Components/BarcodeScanner.vue
@@ -162,7 +162,7 @@ function setActive(scanResult) {
       <!-- Camera Selection Dropdown -->
       <div v-if="cameras.length > 0" class="">
         <InputLabel for="camera" class="">Select Camera:</InputLabel>
-        <select id="camera" v-model="selectedCamera" @change="restartScanner"
+        <select id="camera" v-model="selectedCamera" @change="startScanner"
           class="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6">
           <option v-for="camera in cameras" :key="camera.id" :value="camera.id">
             {{ camera.label || `Camera ${camera.id}` }}
