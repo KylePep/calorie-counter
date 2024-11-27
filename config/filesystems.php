0b2888257e72b6d12d50caeba42fed735fb3c980
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-11-27 12:34:07 -0700

    storagepath as env variable

--- config/filesystems.php
+++ config/filesystems.php
@@ -61,7 +61,7 @@ return [
             'driver' => 'gcs',
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'), 
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET'), 
-            'key_file' => storage_path('app/alien-proton-442704-s1-f67ffb8a28d1.json') ?? json_decode(env('GOOGLE_CLOUD_KEY_FILE')),
+            'key_file' => storage_path('GOOGLE_CLOUD_KEY_FILE'),
             'prefix' => env('GOOGLE_CLOUD_STORAGE_PREFIX', null),
             'visibility' => null, // Disable ACLs
         ],
