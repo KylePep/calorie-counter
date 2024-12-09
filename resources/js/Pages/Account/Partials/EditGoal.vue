b5168ce5081e3ed57838d685bcdebd679caa0439
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-12-09 14:22:39 -0700

    EditGoal uses conditional rendering to prevent miss clicks and better user interactions

--- resources/js/Pages/Account/Partials/EditGoal.vue
+++ resources/js/Pages/Account/Partials/EditGoal.vue
@@ -4,6 +4,8 @@ import Pop from "@/utils/Pop.js";
 import { useForm } from "@inertiajs/vue3";
 import { computed } from "vue";
 import NumberInput from "@/Components/Form/NumberInput.vue";
+import { ref } from "vue";
+import SecondaryButton from "@/Components/Form/SecondaryButton.vue";
 
 
 const props = defineProps(['account']);
@@ -14,6 +16,8 @@ const heightFeet = computed(() => Math.floor((props.account?.height ?? 177.8) / 
 const heightInches = computed(() => (props.account?.height ?? 177.8) / 2.54 % 12 ?? 10);
 
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+const edit = ref(false);
 
 const form = useForm({
   goal: account.value?.goal || 2000,
@@ -70,89 +74,112 @@ const updateAccount = () => {
 
   <div class="p-4 sm:p-8 bg-main border-2 border-light rounded-lg shadow-xl p-12">
 
-    <div v-if="!account?.goal" class="max-w-xl">
+    <div class="space-y-3">
+      <h1 class="font-bold">{{ !account?.goal ? 'No calorie goal yet' : 'Current Calorie Goal:' }} <span>{{
+        props.account?.goal }}</span> </h1>
+      <PrimaryButton v-if="!edit" @click="edit = true">{{ !account?.goal ? 'Create' : 'Change' }} Goal</PrimaryButton>
+      <SecondaryButton v-else @click="edit = false">Cancel</SecondaryButton>
 
-
-      <div class="max-w-xl my-3">
-        <form @submit.prevent="createOrUpdateAccount" class="grid grid-cols-2 gap-3 justify-start">
-
-          <div class="flex flex-col">
-            <h1 class="font-bold">
-              Set Basal Metabolic Rate:
-            </h1>
-            <NumberInput v-model.number="form.bmr" type="number" name="bmr" id="bmr" placeholder="2000"
-              inputmode="numeric" pattern="^[0-9]*$" min="1000" max="20000" class="py-1" />
-          </div>
-
-          <div class="flex flex-col">
-            <h1 class="font-bold">
-              Set Goal Modifier:
-            </h1>
-            <div class="relative">
-              <NumberInput v-model.number="form.goalModifier" type="number" name="goalModifier" id="goalModifier"
-                placeholder="100" inputmode="numeric" pattern="^[0-9]*$" min="50" max="500" class="w-full py-1" />
-              <span class="absolute top-0 right-0 px-4 py-1 font-bold text-neutral/75">%</span>
-            </div>
-          </div>
-
-          <div class="col-span-2 font-bold">
-            Goal: {{ Math.round(form.bmr * (form.goalModifier * .01)) }}
-          </div>
-          <div class=" col-span-2 space-y-3">
-            <PrimaryButton>Create</PrimaryButton>
-            <div class="flex space-x-2">
-              <p>or</p>
-              <a type="button" :href="route('calculator')">Calculate Goal
-              </a>
-            </div>
-          </div>
-
-        </form>
-      </div>
-    </div>
-
-    <div v-else class="max-w-xl">
-      <h1 class="font-bold">
-        Current Calorie Goal:
-      </h1>
-
-      <div class="max-w-xl my-3">
-        <form @submit.prevent="updateAccount" id="updateAccount" class="grid grid-cols-2 gap-3 justify-start ">
+      <div v-if="account?.goal && !edit" class="max-w-xl my-3">
+        <div id="updateAccount" class="grid grid-cols-2 gap-3 justify-start ">
 
           <div class="flex flex-col">
             <h1>BMR</h1>
-            <NumberInput v-model.number="form.bmr" class="py-1" type="number" name="bmr" id="bmr" placeholder="2000"
-              inputmode="numeric" pattern="^[0-9]*$" min="1000" max="20000" />
+            <p>{{ props.account.bmr }}</p>
           </div>
 
           <div class="flex flex-col">
             <h1>Goal Modifier</h1>
-            <NumberInput v-model.number="form.goalModifier" type="number" name="goalModifier" id="goalModifier"
-              placeholder="2000" inputmode="numeric" pattern="^[0-9]*$" min="50" max="500" class="py-1" />
+            <p>{{ props.account.goalModifier }}</p>
           </div>
 
-          <div class="col-span-2 font-bold">
-            Goal: {{ Math.round(form.bmr * (form.goalModifier * .01)) }}
-          </div>
+        </div>
+      </div>
+    </div>
+
+    <div v-if="edit">
+
+      <div v-if="!account?.goal" class="max-w-xl">
 
 
+        <div class="max-w-xl my-3">
+          <form @submit.prevent="createOrUpdateAccount" class="grid grid-cols-2 gap-3 justify-start">
 
-          <div class="col-span-2 space-y-3">
-            <PrimaryButton type="submit" form="updateAccount">
-              Update
-            </PrimaryButton>
-
-            <div class="flex space-x-2">
-              <p>or</p>
-              <a type="button" class=" " :href="route('calculator')">Recalculate
-              </a>
+            <div class="flex flex-col">
+              <h1 class="font-bold">
+                Set Basal Metabolic Rate:
+              </h1>
+              <NumberInput v-model.number="form.bmr" type="number" name="bmr" id="bmr" placeholder="2000"
+                inputmode="numeric" pattern="^[0-9]*$" min="1000" max="20000" class="py-1" />
             </div>
-          </div>
-        </form>
+
+            <div class="flex flex-col">
+              <h1 class="font-bold">
+                Set Goal Modifier:
+              </h1>
+              <div class="relative">
+                <NumberInput v-model.number="form.goalModifier" type="number" name="goalModifier" id="goalModifier"
+                  placeholder="100" inputmode="numeric" pattern="^[0-9]*$" min="50" max="500" class="w-full py-1" />
+                <span class="absolute top-0 right-0 px-4 py-1 font-bold text-neutral/75">%</span>
+              </div>
+            </div>
+
+            <div class="col-span-2 font-bold">
+              Goal: {{ Math.round(form.bmr * (form.goalModifier * .01)) }}
+            </div>
+            <div class=" col-span-2 space-y-3">
+              <PrimaryButton>Create</PrimaryButton>
+              <div class="flex space-x-2">
+                <p>or</p>
+                <a type="button" :href="route('calculator')">Calculate Goal
+                </a>
+              </div>
+            </div>
+
+          </form>
+        </div>
+      </div>
+
+      <div v-else class="max-w-xl">
+
+        <div class="max-w-xl my-3">
+          <form @submit.prevent="updateAccount" id="updateAccount" class="grid grid-cols-2 gap-3 justify-start ">
+
+            <div class="flex flex-col">
+              <h1>BMR</h1>
+              <NumberInput v-model.number="form.bmr" class="py-1" type="number" name="bmr" id="bmr" placeholder="2000"
+                inputmode="numeric" pattern="^[0-9]*$" min="1000" max="20000" />
+            </div>
+
+            <div class="flex flex-col">
+              <h1>Goal Modifier</h1>
+              <NumberInput v-model.number="form.goalModifier" type="number" name="goalModifier" id="goalModifier"
+                placeholder="100" inputmode="numeric" pattern="^[0-9]*$" min="50" max="500" class="py-1" />
+            </div>
+
+            <div class="col-span-2 font-bold">
+              Goal: {{ Math.round(form.bmr * (form.goalModifier * .01)) }}
+            </div>
+
+
+
+            <div class="col-span-2 space-y-3">
+              <PrimaryButton type="submit" form="updateAccount">
+                Update
+              </PrimaryButton>
+
+              <div class="flex space-x-2">
+                <p>or</p>
+                <a type="button" class=" " :href="route('calculator')">Recalculate
+                </a>
+              </div>
+            </div>
+          </form>
+        </div>
+
       </div>
 
     </div>
-
 
   </div>
 </template>
