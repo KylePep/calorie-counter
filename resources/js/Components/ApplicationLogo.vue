7973df0b2054e79eb3c45c933cb37846ed28a3c0
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-12-14 16:32:23 -0700

    lazy loading on icon

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -2,7 +2,7 @@
     <div
         class="bg-gradient-to-b from-accent via-accent-light to-accent rounded-xl border-4 border-dark h-full w-full shadow-xl">
         <!-- <i class="mdi mdi-counter text-shadow flex"></i> -->
-        <img src="/logo.png" alt="mdi mdi-counter text-shadow flex"
-            class="object-center object-contain h-full mx-auto ">
+        <img loading="lazy" src="/logo.png" alt="mdi mdi-counter text-shadow flex"
+            class="object-center object-contain h-full w-full mx-auto ">
     </div>
 </template>
