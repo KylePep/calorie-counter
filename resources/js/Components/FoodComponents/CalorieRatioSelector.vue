5c4814bacc175be727dfe5268754013d828bb49c
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-12-15 20:17:15 -0700

    Created CalorieRatioSelector component, added to calorieSchedule

---
+++ resources/js/Components/FoodComponents/CalorieRatioSelector.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+
+<template>
+  <div class="bg-light p-2 rounded">
+    bar that you drag around to select calories
+  </div>
+</template>
--- resources/js/Pages/Dashboard/partial/CalorieSchedule.vue
+++ resources/js/Pages/Dashboard/partial/CalorieSchedule.vue
@@ -1,18 +1,40 @@
 <script setup>
+import CalorieRatioSelector from "@/Components/FoodComponents/CalorieRatioSelector.vue";
+import UsdaSearch from "@/Components/FoodComponents/UsdaSearch.vue";
+
 
 const props = defineProps(['account', 'foodItems'])
 </script>
 
 
 <template>
-  <div class="text-neutral-text p-4 lg:p-8">
+  <div class="text-neutral-text p-4 lg:p-8 space-y-8">
+
     <h1 class="text-2xl font-bold  mb-3">
       Calorie Schedule
     </h1>
+
     <h2>
       Designate where you would like your calories to go.
     </h2>
-    <div>Find Meals that work for you.</div>
+
+
+
+    <div>
+      Decide and set calorie ratios - Breakfast, Lunch, Dinner, Snack (Beverage)
+    </div>
+
+    <CalorieRatioSelector />
+
+    <div>
+      Find Meals that work for you.
+    </div>
+
+    <div>
+      Based on the ratios derived from your goal/bmr you can then search for food items from the apps ecosystem to help
+      meet your goals.
+    </div>
+    <UsdaSearch />
   </div>
 </template>
 
