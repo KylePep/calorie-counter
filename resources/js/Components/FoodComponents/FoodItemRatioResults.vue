04b50166be95f08b6d037d8c9460f391416be926
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-12-15 21:01:09 -0700

    Css on FoodItemRatioResults

--- resources/js/Components/FoodComponents/FoodItemRatioResults.vue
+++ resources/js/Components/FoodComponents/FoodItemRatioResults.vue
@@ -3,6 +3,14 @@
 
 <template>
   <div>
-    <slot />
+
+    <div class="bg-light text-dark-text px-2 py-1 rounded">
+      <slot />
+    </div>
+
+    <div>
+      Results
+    </div>
+
   </div>
 </template>
